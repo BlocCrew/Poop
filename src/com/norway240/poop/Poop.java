fdebfaca4e93d45a098e2dca87af4393f043f5ee
norway240 <conman240@gmail.com>
2014-07-09 16:07:41 -0500

    EXPLOSIVE DIARRHEA

--- src/com/norway240/poop/Poop.java
+++ src/com/norway240/poop/Poop.java
@@ -3,32 +3,39 @@ package com.norway240.poop;
 import java.util.ArrayList;
 
 import org.bukkit.DyeColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.material.Wool;
+import org.bukkit.material.Dye;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Poop extends JavaPlugin implements Listener {
 	public void onEnable() {
-		getLogger().info("Poop has been enabled!");
+		getLogger().info("Poop has been enabled! (a plugin by norway240)");
 		getServer().getPluginManager().registerEvents(this, this);
 	}
   
 	public void onDisable() {
-		getLogger().info("Poop has been disabled!");
+		getLogger().info("Poop has been disabled! (a plugin by norway240)");
 	}
 	
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onEat(PlayerItemConsumeEvent event){
-		ItemStack isPoop = new ItemStack(Material.WOOL,1);
-		Wool woolPoop = new Wool(DyeColor.BROWN);
-		isPoop.setDurability(woolPoop.getData());		
+		ItemStack isPoop = new ItemStack(Material.INK_SACK,1);
+		Dye dyePoop = new Dye();
+		dyePoop.setColor(DyeColor.BROWN);
+		isPoop.setDurability(dyePoop.getData());		
 		ItemMeta imPoop = isPoop.getItemMeta();
 		imPoop.setDisplayName("Poop");
 		ArrayList<String> lorePoop = new ArrayList<String>();
@@ -37,9 +44,10 @@ public class Poop extends JavaPlugin implements Listener {
 		imPoop.setLore(lorePoop);
 		isPoop.setItemMeta(imPoop);
 		
-		ItemStack isPee = new ItemStack(Material.WOOL,1);
-		Wool woolPee = new Wool(DyeColor.YELLOW);
-		isPee.setDurability(woolPee.getData());
+		ItemStack isPee = new ItemStack(Material.INK_SACK,1);
+		Dye dyePee = new Dye();
+		dyePee.setColor(DyeColor.YELLOW);
+		isPee.setDurability(dyePee.getData());
 		ItemMeta imPee = isPoop.getItemMeta();
 		imPee.setDisplayName("Pee");
 		ArrayList<String> lorePee = new ArrayList<String>();
@@ -48,19 +56,49 @@ public class Poop extends JavaPlugin implements Listener {
 		imPee.setLore(lorePee);
 		isPee.setItemMeta(imPee);
 		
+		ItemStack isDiarrhea = new ItemStack(Material.INK_SACK,1);
+		Dye dyeDiarrhea = new Dye();
+		dyeDiarrhea.setColor(DyeColor.GREEN);
+		isDiarrhea.setDurability(dyeDiarrhea.getData());
+		ItemMeta imDiarrhea = isDiarrhea.getItemMeta();
+		imDiarrhea.setDisplayName("Diarrhea");
+		ArrayList<String> loreDiarrhea = new ArrayList<String>();
+		loreDiarrhea.add("Get to the");
+		loreDiarrhea.add("BATHROOM!");
+		imDiarrhea.setLore(loreDiarrhea);
+		isDiarrhea.setItemMeta(imDiarrhea);
+		
 		Player player = event.getPlayer();
 		ItemStack item = event.getItem();
 		int itemID = item.getTypeId();
 		
-		if (itemID == 373){
+		if ((itemID == 373)||(itemID == 335)){
 			player.getInventory().addItem(new ItemStack(isPee));
 			player.sendMessage("You will now need to pee");
-			
+		}else if(itemID == 367){
+			PotionEffect pe = new PotionEffect(PotionEffectType.CONFUSION, 600, 1);
+			player.addPotionEffect(pe);
+			player.getInventory().addItem(new ItemStack(isDiarrhea));
+			player.sendMessage("I think you need to go to the bathroom");
 		}else{
 			player.getInventory().addItem(new ItemStack(isPoop));
 			player.sendMessage("You will now need to poop");
 		}
 		
+	}
+
+	@EventHandler
+	public void onDrop(PlayerDropItemEvent event){
+		Player player = event.getPlayer();
+		Location loc = player.getLocation();
+		World world = loc.getWorld();
+		Item item = event.getItemDrop();
+		ItemStack is = item.getItemStack();
+		String name = is.getItemMeta().getDisplayName().toString();
+		if (name.equalsIgnoreCase("Diarrhea")){
+			world.createExplosion(loc, 0);
+			player.sendMessage("You might want to talk to a doctor...");
+		}
 	}
 	
 }
